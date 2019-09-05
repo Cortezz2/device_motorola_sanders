1291a4e5eb8086172ecb67700ef58bdb4f697683
Cortezz2 <36489996+Cortezz2@users.noreply.github.com>
2019-09-04 22:22:15 -0300

    Update and rename aosp_sanders.mk to aosdp_sanders.mk

--- aosdp_sanders.mk
+++ aosdp_sanders.mk
@@ -20,11 +20,11 @@ $(call inherit-product, device/motorola/sanders/full_sanders.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosdp/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-PRODUCT_NAME := aosp_sanders
+PRODUCT_NAME := aosdp_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
